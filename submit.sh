ca24983577e3500877970a322b902062a3751713
Savannah <savannah.ferretti@uci.edu>
2026-04-20 15:20:25 -0700

    New scripts

--- submit.sh
+++ submit.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 #SBATCH --account=m4334
 #SBATCH --constraint=cpu
+#SBATCH --qos=shared
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=128
 #SBATCH --time=06:00:00
-#SBATCH --job-name=pysr_${RUNNAME}
-#SBATCH --output=logs/pysr_%x_%j.out
-#SBATCH --error=logs/pysr_%x_%j.err
+#SBATCH --job-name=${RUNNAME}
+#SBATCH --output=logs/%x_%j.log
 
 export OMP_NUM_THREADS=1
 export JULIA_NUM_THREADS=1
 export JULIA_DEPOT_PATH=/pscratch/sd/s/sferrett/.julia
 export PYTHON_JULIAPKG_PROJECT=/pscratch/sd/s/sferrett/.julia/environments/pyjuliapkg
 
-mkdir -p logs
-
-module load conda
+module load python conda
+source "$(conda info --base)/etc/profile.d/conda.sh"
 conda activate monsoon-discovery
-
-cd /global/homes/s/sferrett/monsoon-discovery
 
 python -m scripts.models.sr.train \
     --runs ${RUNNAME:-all} \
