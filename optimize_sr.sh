149a569c93006ed948a639b82f9a5667279c9f79
Savannah <savannah.ferretti@uci.edu>
2026-04-29 08:49:26 -0700

    Cleaning

--- optimize_sr.sh
+++ optimize_sr.sh
@@ -18,10 +18,10 @@ module load python conda
 source "$(conda info --base)/etc/profile.d/conda.sh"
 conda activate monsoon-discovery
 
-EQ=${1:-all}
-SPLITS=${2:-valid}
+# EQ=${1:-all}
+# SPLITS=${2:-valid}
 
 scontrol update JobId=${SLURM_JOB_ID} Name=${EQ}
-echo "Optimizing equations: ${EQ}  |  splits: ${SPLITS}  |  workers: ${SLURM_CPUS_PER_TASK}"
+# echo "Optimizing equations: ${EQ}  |  splits: ${SPLITS}  |  workers: ${SLURM_CPUS_PER_TASK}"
 
-python -m scripts.models.sr.optimize --equations ${EQ} --splits ${SPLITS}
+python -m scripts.models.sr.optimize --equations all --splits valid
