310601317cc733740e320b0731b66e3d1ad53bf0
Savannah <savannah.ferretti@uci.edu>
2026-05-17 14:18:12 -0700

    New results

--- optimize_sr.sh
+++ optimize_sr.sh
@@ -5,7 +5,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=32
-#SBATCH --time=00:10:00
+#SBATCH --time=00:20:00
 #SBATCH --job-name=optimize_sr
 #SBATCH --output=logs/%x_%j.log
 
@@ -24,4 +24,4 @@ conda activate monsoon-discovery
 # scontrol update JobId=${SLURM_JOB_ID} Name=${EQ}
 # echo "Optimizing equations: ${EQ}  |  splits: ${SPLITS}  |  workers: ${SLURM_CPUS_PER_TASK}"
 
-python -m scripts.models.sr.optimize --equations all --splits test
+python -m scripts.models.sr.optimize --equations sr_hi --splits test
