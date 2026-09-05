5641b87d4a758bbfc6be024babb2f4f0103eb8cb
Savannah <savannah.ferretti@uci.edu>
2026-09-04 17:52:58 -0700

    New runs

--- train_sr.sh
+++ train_sr.sh
@@ -32,17 +32,17 @@ echo "Training model: ${RUN}"
 
 python -m scripts.models.sr.train --runs ${RUN} "$@"
 
-# scancel $(squeue -u sferrett -h -o "%i") 2>/dev/null
-# find /global/homes/s/sferrett/ -name "lock.pid" -delete 2>/dev/null
-# salloc --account=m4334 --constraint=cpu --qos=interactive --nodes=1 --time=04:00:00
-# cd /global/cfs/cdirs/m4334/sferrett/monsoon-discovery
-# rsync -a --update /global/cfs/cdirs/m4334/sferrett/.julia/ $SCRATCH/.julia/
-# export OMP_NUM_THREADS=1
-# export JULIA_NUM_THREADS=1
-# export JULIA_DEPOT_PATH=$SCRATCH/.julia
-# export PYTHON_JULIAPKG_PROJECT=$SCRATCH/.julia/environments/pyjuliapkg
-# export UCX_ERROR_SIGNALS=""
-# module load python conda
-# source "$(conda info --base)/etc/profile.d/conda.sh"
-# conda activate monsoon-discovery
-# python -m scripts.models.sr.train --runs <run_name> --procs 50
+scancel $(squeue -u sferrett -h -o "%i") 2>/dev/null
+find /global/homes/s/sferrett/ -name "lock.pid" -delete 2>/dev/null
+salloc --account=m4334 --constraint=cpu --qos=interactive --nodes=1 --cpus-per-task=52 --time=04:00:00
+cd /global/cfs/cdirs/m4334/sferrett/monsoon-discovery
+rsync -a --update /global/cfs/cdirs/m4334/sferrett/.julia/ $SCRATCH/.julia/
+export OMP_NUM_THREADS=1
+export JULIA_NUM_THREADS=1
+export JULIA_DEPOT_PATH=$SCRATCH/.julia
+export PYTHON_JULIAPKG_PROJECT=$SCRATCH/.julia/environments/pyjuliapkg
+export UCX_ERROR_SIGNALS=""
+module load python conda
+source "$(conda info --base)/etc/profile.d/conda.sh"
+conda activate monsoon-discovery
+python -m scripts.models.sr.train --runs sr_all --procs 50
