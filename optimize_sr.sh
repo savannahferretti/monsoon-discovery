e2c7cb021ac070b0829c74fcaf3da949061fcc41
Savannah <savannah.ferretti@uci.edu>
2026-09-11 12:41:59 -0700

    Cleaning, still debugging...

--- optimize_sr.sh
+++ optimize_sr.sh
@@ -5,7 +5,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=32
-#SBATCH --time=00:45:00
+#SBATCH --time=00:20:00
 #SBATCH --job-name=optimize_sr
 #SBATCH --output=logs/%x_%j.log
 
@@ -22,6 +22,5 @@ EQ=${1:-all}
 SPLITS=${2:-test}
 
 scontrol update JobId=${SLURM_JOB_ID} Name=${EQ}
-echo "Optimizing equations: ${EQ}  |  splits: ${SPLITS}  |  workers: ${SLURM_CPUS_PER_TASK}"
 
 python -m scripts.models.sr.optimize --equations ${EQ} --splits ${SPLITS}
