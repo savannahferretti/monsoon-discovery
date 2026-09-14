696bd81dc2ab1e91b5249e4800f208f1e7c53c8c
Savannah <savannah.ferretti@uci.edu>
2026-09-14 16:25:34 -0700

    New

--- optimize_sr.sh
+++ optimize_sr.sh
@@ -19,8 +19,7 @@ source "$(conda info --base)/etc/profile.d/conda.sh"
 conda activate monsoon-discovery
 
 EQ=${1:-all}
-SPLITS=${2:-test}
 
 scontrol update JobId=${SLURM_JOB_ID} Name=${EQ}
 
-python -m scripts.models.sr.optimize --equations ${EQ} --splits ${SPLITS}
+python -m scripts.models.sr.optimize --equations ${EQ} --splits test
